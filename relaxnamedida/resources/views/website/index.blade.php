7a047e5daf8ffa7aea24dea2502cd79644ed21b3
Vinicio Gabriel <vinicio@cafecomideias.com.br>
2016-03-23 23:07:57 -0300

    Completion carousel products, creating form and other creations and corrections.

--- relaxnamedida/resources/views/website/index.blade.php
+++ relaxnamedida/resources/views/website/index.blade.php
@@ -152,7 +152,60 @@ Contact: hello@brunomartins.com
         <div class="margin-top-40 margin-bottom-30"></div>
         <!-- Step 01 -->
         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-            <img src="{{ asset('assets/images/step-2.png') }}" class="img-responsive">
+            <div id="carousel-example-generic" class="carousel slide products" data-ride="carousel">
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="{{ asset('assets/images/_upload/products/A-Z-Small.png') }}" alt="A-Z-Small">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/A-Z-Big.png') }}" alt="A-Z-Big">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/BioSoak-Big.png') }}" alt="BioSoakBig">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/BioSoak-Small.png') }}" alt="BioSoakBig">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/Cicatriderm-Big.png') }}" alt="Cicatriderm-Big">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/Cicatriderm-Small.png') }}" alt="Cicatriderm-Small">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/MaxAir-Big.png') }}" alt="MaxAir-Big">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/MaxAir-Small.png') }}" alt="MaxAir-Small">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/Segurdent-Big.png') }}" alt="Segurdent-Big">
+                    </div>
+                    <div class="item">
+                        <img src="{{ asset('assets/images/_upload/products/Segurdent-Small.png') }}" alt="Segurdent-Small">
+                    </div>
+                </div>
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="8"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="9"></li>
+                </ol>
+            </div>
+            <div class="margin-top-40 margin-bottom-30">
+
+
+            </div>
+
             <h2 class="text-orange text-uppercase font-size-36 strong padding-top-10">1° Passo</h2>
             <p class="font-size-16">É fácil. Basta comprar um dos produtos:
                 Max Air, Biosoak (480ml), Cicatriderm, A-Z polivitamínico e ou Linha Segurdent.</p>
@@ -192,7 +245,106 @@ Contact: hello@brunomartins.com
             <h2 class="text-yellow text-uppercase font-size-36 strong padding-top-10">Quero Participar</h2>
             <h3 class="text-white font-size-36 lobster-two">e concorrer a viagem dos sonhos.</h3>
         </header>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 margin-top-45 margin-bottom-35 font-size-18">
+            Preencha corretamente o cadastro de participação. Tudo preenchido e enviado, vá até o seu e-mail e confirme sua
+            inscrição. Faça seu login, envie uma foto do cupom fiscal dos produtos Teuto participantes e responda à pergunta
+            "Por que a melhor forma de ficar relax é com o Teuto?". Os autores das 3 melhores respostas serão premiados com
+            um vale-viagem cada um.
+        </div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 margin-top-35 margin-bottom-35">
+            <div class="tabbable participation"> <!-- Only required for left/right tabs -->
+                <ul class="nav nav-tabs lobster-two font-size-36">
+                    <li class="active"><a href="#tab1" data-toggle="tab">Dados Pessoais</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Endereço</a></li>
+                    <li><a href="#tab3" data-toggle="tab">Conclusão</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="tab1">
+                    <form>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 margin-top-35 font-size-18">
+                            <div class="col-lg-5 col-md-5 col-xs-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Nome">
+                                </div>
+                            </div>
+                            <div class="col-lg-5 col-md-5 col-xs-12">
+                                <div class="form-group">
+                                    <input type="email" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-xs-12">
+                                <div class="form-group">
+                                    <input type="tel" class="form-control" placeholder="Telefone">
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-xs-12">
+                                <div class="form-group">
+                                    <input type="tel" class="form-control" placeholder="Telefone Celular">
+                                </div>
+                            </div>
+                            <div class="col-lg-5 col-md-5 col-xs-12">
+                                <label>Sexo:</label>
+                                <label for="male" class="radio-inline">
+                                    <input type="radio" name="male" id="male" value="masculino"> Masculino
+                                </label>
+                                <label for="female" class="radio-inline">
+                                    <input type="radio" name="female" id="female" value="feminino"> Feminino
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 font-size-18">
+                            <div class="col-lg-3 col-md-3 col-xs-12">
+                                <div class="form-group">
+                                    <input type="date" class="form-control" placeholder="Data de Nascimento">
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-xs-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="CPF">
+                                </div>
+                            </div>
+                            <div class="col-lg-5 col-md-5 col-xs-12">
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" id="inlineCheckbox1" value="authority"> Você é autoridade governamental?
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 font-size-18">
+                            <div class="col-lg-3 col-md-3 col-xs-12">
+                                <div class="form-group">
+                                    <input type="password" class="form-control" placeholder="Senha">
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-xs-12">
+                                <div class="form-group">
+                                    <input type="password" class="form-control" placeholder="Confirmar Senha">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-lg-offset-2 col-md-4 col-xs-12">
+                                <div class="form-group">
+                                    <button class="btn-main" title="Avançar">Próximo Passo</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                    </div>
+                    <div class="tab-pane" id="tab2">
+                        <p>Aguardando conteúdo na seção 02</p>
+                    </div>
+                    <div class="tab-pane" id="tab3">
+                        <p>Aguardando conteúdo na seção 03</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </article>
+
+
+
 </section>
 
 <link rel="stylesheet" href="{!! asset('assets/css/bootstrap.min.css') !!}" />
@@ -204,6 +356,8 @@ Contact: hello@brunomartins.com
 @if(Session::has('message'))
 <script>
 alert('{!! Session::get('message') !!}');
+
+$('.carousel').carousel();
 </script>
 @endif
 @yield('javascript')
